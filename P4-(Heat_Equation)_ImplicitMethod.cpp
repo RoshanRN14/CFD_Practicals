d53aaaf6de441aaa54ed7785f01621ff3570dda1
RoshanRN14 <137896509+RoshanRN14@users.noreply.github.com>
2024-11-10 00:11:30 +0530

    Update P4-(Heat_Equation)_ImplicitMethod.cpp

--- P4-(Heat_Equation)_ImplicitMethod.cpp
+++ P4-(Heat_Equation)_ImplicitMethod.cpp
@@ -1,53 +1,73 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <iomanip>
 using namespace std;
+
 float x[10];
-float *tridiagonal(float r, float d[],int n)
-{
+
+// Thomas algorithm for tridiagonal matrix solution
+float* tridiagonal(float a, float b, float c, float d[], int n) {
     int i;
-    float a=-r,b=(1+2*r),c=-r;
-    float alpha[10],beta[10];
-    alpha[1]=b;
-    beta[1]=d[1]/b;
-    for(i=2;i<n;i++)
-    {
-        alpha[i]=b-(a*c/alpha[i-1]);
-        beta[i]=(d[i]-a*beta[i-1])/alpha[i];
+    float alpha[10], beta[10];
+    
+    // Initialize the first entries
+    alpha[1] = b;
+    beta[1] = d[1] / b;
+    
+    // Forward elimination
+    for (i = 2; i < n - 1; i++) {
+        alpha[i] = b - (a * c / alpha[i - 1]);
+        beta[i] = (d[i] - a * beta[i - 1]) / alpha[i];
     }
-    x[n-1]=beta[n-1];
-    for(i=n-2;i>=1;i--)
-        x[i]=beta[i]-(c*x[i+1])/alpha[i];
+
+    // Back substitution
+    x[n - 2] = beta[n - 2];
+    for (i = n - 3; i >= 1; i--) {
+        x[i] = beta[i] - (c * x[i + 1]) / alpha[i];
+    }
+
     return x;
 }
-int main()
-{
-    int i,j,n,m;
-    float u[100][100],h,k,c,r;
-    r=0.5;
-    m=5;
-    n=8;
-    for(j=0;j<=m;j++)
-    {
-        u[0][j]=0;
-        u[n][j]=0;
+
+int main() {
+    int i, j, n = 8, m = 5;
+    float u[100][100], r = 0.5;
+
+    // Boundary and initial conditions
+    for (j = 0; j <= m; j++) {
+        u[0][j] = 0;
+        u[n][j] = 0;
     }
-    for(i=1;i<n;i++)
-        u[i][0]=(i*(8-i))/2.0;
+    for (i = 1; i < n; i++) {
+        u[i][0] = (i * (8 - i)) / 2.0;  // Initial temperature distribution
+    }
+
+    float a = -r, b = (1 + 2 * r), c = -r;
     float d[n];
-    for(j=0;j<m;j++)
-    {
-        for(i=1;i<n;i++)
-            d[i]=r*u[i-1][j]+2*(1-r)*u[i][j+1]+r*u[i+1][j];
-        float *y=tridiagonal(r,d,n);
-        for(i=1;i<n;i++)
-            u[i][j+1]=y[i];
+
+    for (j = 0; j < m; j++) {
+        // Set up the right-hand side for the implicit scheme at time step j
+        for (i = 1; i < n - 1; i++) {
+            d[i] = u[i][j];  // Right-hand side is u[i][j] from previous time level
+        }
+
+        // Solve the tridiagonal system for the next time step
+        float* y = tridiagonal(a, b, c, d, n);
+
+        // Store results for the next time step in u
+        for (i = 1; i < n - 1; i++) {
+            u[i][j + 1] = y[i];
+        }
     }
-    cout<<"The value of all u(i,j): \n";
-    cout<<fixed<<setprecision(4);
-    for(j=m;j>=0;j--)
-    {
-        for(i=0;i<=n;i++)
-           cout<<"\t"<<u[i][j];
-       cout<<endl;
+
+    // Output the result
+    cout << "The value of all u(i,j):\n";
+    cout << fixed << setprecision(4);
+    for (j = m; j >= 0; j--) {
+        for (i = 0; i <= n; i++) {
+            cout << "\t" << u[i][j];
+        }
+        cout << endl;
     }
+
+    return 0;
 }
